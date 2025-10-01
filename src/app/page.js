c76b952331944570b41aa88b1d5a4fdcc312527c
KeviinDavis <daviskevin1@hotmail.com>
2025-10-01 10:49:07 -0400

    collections component add

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,8 @@ import Hero from '../components/Hero/index.jsx'
 import FeaturedProduct from '../components/FeaturedProduct/index.jsx'
 import Philosophy from '../components/Philosophy/index.jsx'
 import About from '../components/About/index.jsx'
+import Collections from '../components/Collections/index.jsx'
+
 
 export default function Home() {
   return (
@@ -11,6 +13,7 @@ export default function Home() {
       <Hero />
       <Philosophy />
       <About />
+      <Collections />
       <FeaturedProduct />
     </div>
   );
---
+++ src/components/Collections/index.jsx
@@ -0,0 +1,58 @@
+'use client';
+import styles from './Collections.module.css';
+import Link from 'next/link';
+import { useEffect, useRef } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function Collections() {
+  const imageRefs = useRef([]);
+
+  useEffect(() => {
+    if (window.innerWidth < 768) return; // Disable on mobile
+
+    imageRefs.current.forEach((el, i) => {
+      const speed = [0.95, 3.75, 2][i]; // Custom speed per image
+
+      gsap.to(el, {
+        yPercent: -20 * speed,
+        ease: 'none',
+        scrollTrigger: {
+          trigger: el,
+          start: 'top bottom', // when image enters viewport
+          scrub: true,
+        },
+      });
+    });
+
+    return () => {
+      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+    };
+  }, []);
+
+  const imageData = [
+    { src: '/images/outsidearea.png', label: 'OUTSIDE' },
+    { src: '/images/kitchenarea.png', label: 'KITCHEN' },
+    { src: '/images/livingarea.png', label: 'LIVING' },
+  ];
+
+    return (
+    <section className={styles.collectionsWrapper}>
+        {imageData.map((item, index) => (
+        <Link href="/products" key={index} className={styles.collectionItem}>
+            <div
+            className={styles.parallaxGroup}
+            ref={el => (imageRefs.current[index] = el)}
+            >
+            <div className={styles.imageContainer}>
+                <img src={item.src} alt={item.label} />
+            </div>
+            <h4>{item.label}</h4>
+            </div>
+        </Link>
+        ))}
+    </section>
+    );
+}
