74e61f9b832acf8a73522eee7db70f94dead3750
KeviinDavis <daviskevin1@hotmail.com>
2025-10-01 13:29:52 -0400

    about page complete

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,13 +1,16 @@
 import AboutHero from '../../components/AboutHero';
 import AboutHistory from '../../components/AboutHistory';
 import AboutImage from '../../components/AboutImage';
+import FeaturedProduct from '../../components/FeaturedProduct';
 
 export default function AboutPage() {
   return (
     <main>
+        {/* <AboutImage /> */}
         <AboutHero />
         <AboutHistory />
         <AboutImage />
+        {/* <FeaturedProduct /> */}
     </main>
   )
 }
--- src/components/AboutHistory/index.jsx
+++ src/components/AboutHistory/index.jsx
@@ -16,14 +16,14 @@ export default function AboutHistory() {
         <div className={styles.yearBlock}>
           <h4>1975</h4>
           <p>
-            It all started in <strong>1975</strong>, when <strong>Dimitris Rigas</strong>, returning after 7 years from America, conceived the idea of creating dining chairs, inspired by the needs and trends he had observed there. With a vision of quality and durability, he began production in a small workshop, laying the foundations for what was to develop into a pioneering path in the field of professional furniture.
+            It all started in <strong>1975</strong>, when <strong>Stephen ArtHaus</strong>, returning after 7 years from America, conceived the idea of creating dining chairs, inspired by the needs and trends he had observed there. With a vision of quality and durability, he began production in a small workshop, laying the foundations for what was to develop into a pioneering path in the field of professional furniture.
           </p>
         </div>
 
         <div className={styles.yearBlock}>
           <h4>1985</h4>
           <p>
-            Ten years later, in <strong>1985</strong>, a trip to an exhibition in Italy would change his perspective once again. There, the idea of the Viennese chair was born – not wooden, as was traditionally the case, but made of metal. The proposal was radical and highly innovative for the time. The market response was impressive: sales skyrocketed and the metal Viennese chair became synonymous with aesthetics, durability and functionality.
+            Ten years later, in <strong>1985</strong>, a trip to an exhibition in Italy would change his perspective once again. There, the idea of the Viennese chair was born not wooden, as was traditionally the case, but made of metal. The proposal was radical and highly innovative for the time. The market response was impressive: sales skyrocketed and the metal Viennese chair became synonymous with aesthetics, durability and functionality.
           </p>
         </div>
 
@@ -44,10 +44,10 @@ export default function AboutHistory() {
         <div className={styles.yearBlock}>
           <h4>2015</h4>
           <p>
-            In <strong>2015</strong>, in the midst of the economic crisis, the company did not retreat – it evolved. It turned to new communication and commerce channels, launching its first e-shop and strategically focusing on the catering and hotel equipment sector. It was the beginning of a new era.
+            In <strong>2015</strong>, in the midst of the economic crisis, the company did not retreat  it evolved. It turned to new communication and commerce channels, launching its first e-shop and strategically focusing on the catering and hotel equipment sector. It was the beginning of a new era.
           </p>
           <p>
-            And we come to today. The company completes <strong>50 years</strong> of presence in the field of professional furniture. At the helm are now <strong>Kiki and Anna Riga</strong>, who continue the vision and work of their father, <strong>Dimitris Rigas</strong>. With new designs, fresh ideas and modern aesthetics, they are moving forward dynamically, presenting a new site with premium items and an innovative e-shop, bringing to the market unique proposals that meet the requirements of the season.
+            And we come to today. The company completes <strong>50 years</strong> of presence in the field of professional furniture. At the helm are now <strong>Kiki and Anna Riga</strong>, who continue the vision and work of their father, <strong>Stephen ArtHaus</strong>. With new designs, fresh ideas and modern aesthetics, they are moving forward dynamically, presenting a new site with premium items and an innovative e-shop, bringing to the market unique proposals that meet the requirements of the season.
           </p>
           <p><strong>We are here. And we continue…</strong></p>
         </div>
