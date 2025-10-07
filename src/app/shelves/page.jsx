ee4708e480938948e4c36b93b57e16c24b4ec3f7
KeviinDavis <daviskevin1@hotmail.com>
2025-10-07 14:07:15 -0400

    overlay reveal animation (GSAP) implemented over Products Grid component

--- src/app/shelves/page.jsx
+++ src/app/shelves/page.jsx
@@ -1,12 +1,15 @@
 import Hero from '../../components/Hero';
 import ProductsGrid from '../../components/ProductsGrid';
 import shelvesData from '../../data/shelvesData';
+import About from '../../components/About/index.jsx'
+
 
 
 export default function Products() {
   return (
     <main>
       <Hero />
+      <About />
       <ProductsGrid  items={shelvesData}/>
     </main>
   )
--- src/components/ProductsGrid/index.jsx
+++ src/components/ProductsGrid/index.jsx
@@ -1,17 +1,49 @@
+'use client';
+import { useEffect, useRef } from 'react';
 import styles from './ProductsGrid.module.css';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function ProductsGrid({ items }) {
+  const gridRef = useRef(null);
+
+  useEffect(() => {
+    const overlays = gridRef.current.querySelectorAll(`.${styles.imageOverlay}`);
+
+    overlays.forEach((overlay) => {
+      gsap.fromTo(
+        overlay,
+        { xPercent: 0 },
+        {
+          xPercent: 100,
+          duration: 2.6,
+          ease: 'power4.out',
+          scrollTrigger: {
+            trigger: overlay,
+            start: 'top 80%',
+            toggleActions: 'play none none none',
+          },
+        }
+      );
+    });
+  }, []);
+
   return (
     <section className={styles.productsWrapper}>
-      <div className={styles.grid}>
+      <div ref={gridRef} className={styles.grid}>
         {items.map((item, index) => (
           <div className={styles.card} key={index}>
             <div className={styles.imageContainer}>
-              <img
-                src={item.src}
-                alt={item.alt}
-                className={styles.image}
-              />
+              <div className={styles.revealWrapper}>
+                <div className={styles.imageOverlay}></div>
+                <img
+                  src={item.src}
+                  alt={item.alt}
+                  className={styles.image}
+                />
+              </div>
             </div>
             <h4 className={styles.title}>{item.title}</h4>
           </div>
