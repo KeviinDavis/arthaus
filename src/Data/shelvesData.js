962d2be816010398084411309a95450512f3447b
KeviinDavis <daviskevin1@hotmail.com>
2025-10-07 16:02:24 -0400

    Product/Shelves title changes

--- src/Data/shelvesData.js
+++ src/Data/shelvesData.js
@@ -4,44 +4,44 @@ const shelvesData = [
     {
     src: '/images/Products/Shelves/shelflargebrown.webp',
     alt: 'Shelf Large Brown',
-    title: 'Large Brown',
+    title: 'ROMULUS BOOKCASE RECYCLED TEAK WOOD WITH 2 DRAWERS',
     dimensions: '105x32x140',
   },
   {
     src: '/images/Products/Shelves/shelftanwhite.webp',
     alt: 'Shelf Tan White',
-    title: 'Tan White',
+    title: 'STOCKHOLM BOOKCASE SONOMA DECAPE WHITE WITH MARBLE FINISH',
     dimensions: '105x32x140',
   },
     {
     src: '/images/Products/Shelves/shelfuk.webp',
     alt: 'Shelf UK',
-    title: 'UK Shelf',
+    title: 'LONDON CALLING HIGH BOOKCASE RED',
     dimensions: '105x32x140',
   },
   {
     src: '/images/Products/Shelves/shelfbike.webp',
     alt: 'Shelf Bike',
-    title: 'Shelf Bike',
+    title: 'MOTOR HIGH BOOKCASE NATURAL',
     dimensions: '105x32x140',
   },
   {
     src: '/images/Products/Shelves/shelfbrownblack.webp',
     alt: 'Shelf Brown Black',
-    title: 'Brown Black',
+    title: 'BRICK-URBAN BOOKCASE WALNUT BLACK',
     dimensions: '105x32x140',
   },
 
   {
     src: '/images/Products/Shelves/shelftanblack.webp',
     alt: 'Shelf Tan Black',
-    title: 'Tan Black',
+    title: 'STAPLES BOOKCASE 4 SHELVES WHITE WASHED BLACK',
     dimensions: '105x32x140',
   },
     {
     src: '/images/Products/Shelves/shelfblackcurved.webp',
     alt: 'Shelf Black Curved',
-    title: 'Black Curved',
+    title: 'NESSOS BOOKCASE BLACK ',
     dimensions: '105x32x140',
   },
 
